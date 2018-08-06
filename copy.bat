0b7442c39729e87b0072e0d8a6fd2ec034b5e008
ziniulian <ziniulian@163.com>
2018-08-06 11:43:00 +0800

    修改路径

--- copy.bat
+++ copy.bat
@@ -1,5 +1,5 @@
 @echo off
-xcopy L:\Doc\SVN\XC2002\src\XC2002\app\src\main L:\Doc\Git\AppInvXC2002\app\src\main\ /S
-xcopy L:\Doc\SVN\XC2002\src\XC2002\app\libs L:\Doc\Git\AppInvXC2002\app\libs\ /S
-copy L:\Doc\SVN\XC2002\src\XC2002\app\build.gradle L:\Doc\Git\AppInvXC2002\app
+xcopy L:\Doc\SVN\Work\XC2002\src\XC2002\app\src\main L:\Doc\Git\AppInvXC2002\app\src\main\ /S
+xcopy L:\Doc\SVN\Work\XC2002\src\XC2002\app\libs L:\Doc\Git\AppInvXC2002\app\libs\ /S
+copy L:\Doc\SVN\Work\XC2002\src\XC2002\app\build.gradle L:\Doc\Git\AppInvXC2002\app
 pause
