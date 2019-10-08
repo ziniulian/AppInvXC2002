482dc5d76b35f87c968846fa05f3cf01c64739ca
ziniulian <ziniulian@163.com>
2019-10-08 08:53:12 +0800

    更新代码表

--- app/src/main/assets/web/js/about.js
+++ app/src/main/assets/web/js/about.js
@@ -1,6 +1,6 @@
 function init() {
 	rfid.flushTim();
-	content.innerHTML = "产品型号：" + rfid.getAppNam().substr(0, 10) + "<br />公司名称：远望谷<br />软件版本：V0.2.14<br />RFID基带版本：V1.01";
+	content.innerHTML = "产品型号：" + rfid.getAppNam().substr(0, 10) + "<br />公司名称：远望谷<br />软件版本：V0.2.15<br />RFID基带版本：V1.01";
 }
 
 dat = {
